0e293b1693cf37b9b78aa7bc05f322a8e36ab544
Eri <eriinwanderland@calvisitor-10-105-148-148.calvisitor.1918.berkeley.edu>
2017-06-26 17:19:06 -0700

    adding more javascript functions and fixing the on click! Whoo

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,10 @@
+$(function(){
 //array of animals
 var animalArray = ["Cat", "Dog", "Bird", "Honey Badger", "Rabbit"];
 console.log('animalArray', animalArray);
- 
+
+renderButtons();
+
 //function to render buttons
 function renderButtons(){
 	//empty buttons
@@ -16,7 +19,7 @@ function renderButtons(){
 			newButton.attr("data-name", animalArray[i]);
 			//add text to the button
 			newButton.text(animalArray[i]);
-			//prepend buttons to the gifsRatingsDiv
+			//prepend buttons to the animal butttons Div
 			$("#animal-buttons").prepend(newButton);
 		}
 };
@@ -25,9 +28,10 @@ function renderButtons(){
 function displayGIFs(){
 	//var animal
 	var animal = $(this).attr("data-name");
+	console.log(animal);
 	console.log('this', this);
 	//queryurl
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=5";
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 	console.log('queryURL', queryURL);
 	//ajax call
 	$.ajax({
@@ -35,49 +39,38 @@ function displayGIFs(){
 		method: "GET"
 	}).done(function(response){
 		console.log(response);
-			//display the rating and the gif on the HTML
-      		var rating = $("<p>").text("Rating: " + response.rating);
-      		var animalGif = $("<img>").attr("src", response.fixed_height_still);
-      		//empty animals div of gifs and rating info
-      		$("#animals").empty();
-      		//append the rating and gif to the animal Div on HTML
-      		$("#animals").append(rating, animalGif);
-
-			//create new div to hold each animal rating info and gifs
-			// var gifsRatingsDiv = $("<div class ='gifsRatings'>"); 
-			// //pull the rating info
-			// var rating = response.rating;
-			// console.log('response.rating', response.rating);
-			// //create new div to hold/store the response.rating and display it in HTML
-			// var ratingDiv = $("<p>").text("Rating: "+ rating);
-			// //pull ten gifs
-			// var gifs = response.fixed_height_still; 
-			// //display the rating by appending new var to the new div
-			// gifsRatingsDiv.append(ratingDiv);
-			// //append var gifs to the gifsRatingsDiv
-			// gifsRatingsDiv.append(gifs);
-			// //append response and gif divs to the grander HTML div 
-			// $("#animals").prepend(gifsRatingsDiv);
+	      	//empty animals div of gifs and rating info
+	      	$("#animals").empty();
+	      	//variable to hold the response from ajax call
+			var gifs = response.data;
+			//for loop to generate the rating and gifs for each of the objects
+			for(var i = 0; i < gifs.length; i++){
+				//display the rating and the gif on the HTML
+	      		var rating = $("<p>").text("Rating: " + gifs[i].rating);
+	      		var animalGif = $('<img data-state="still">');
+	      		//assign attribute to animalgif var
+	      		animalGif.attr('src', gifs[i].images.fixed_height_still.url)
+	   			animalGif.attr('data-animate', gifs[i].images.fixed_height.url)
+	   			animalGif.attr('data-still', gifs[i].images.fixed_height_still.url)	
+				animalGif.attr('class', 'gif img-responsive')
+	      		//append the rating and gif to the animal Div on HTML
+	      		$("#animals").append(rating, animalGif);
+      		}
 		});
 };
 
-$("#animal-buttons").on("click", function(){
-	//run function to display buttons and gifs
-	displayGIFs();
-});
 
+$(document).on("click", ".new-animals", displayGIFs)
+	
 //on click function on gif 
-$("#animals").on("click", function(){
-	//create variable for gif state 
-	var state = $(this).attr("data-state");
-    console.log(state);
+$(document).on("click", ".gif img-responsive", function(){
 	//create variable for still state
 	var still = $(this).attr("data-still");
 	//create variable for animated state
 	var animate = $(this).attr("data-animate");
 	//conditionals 
 		//if gif is still
-		if(state === 'still'){
+		if($(this).attr("data-state") === "still"){
 			//animate
 			$(this).attr("src", animate);
           	$(this).attr("data-state", "animate");
@@ -90,16 +83,17 @@ $("#animals").on("click", function(){
 
 });
 
-$(function(){
+
 //on click function when user clicks button id=add-animal
 	$("#add-animal").on("click", function(event){
-		event.preventdefault();
+		event.preventDefault();
 		console.log('click work?');
 		//store the animal name from the text box id=animal-input
 		var userInput = $("#animal-input").val().trim();
 		//add new user input to the animal array
 		animalArray.push(userInput);
 		//run function to render buttons
-		// renderButtons();
+		renderButtons();
 	});
 });
+
